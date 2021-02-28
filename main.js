2fffce52434f67744b96c9a6fa37914866b6b370
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-02-28 01:48:12 -0800

    patch fix

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 let result1 = document.getElementById('result1');
 let result2 = document.getElementById('result2');
 let battleText = document.getElementById('battleText');
-<<<<<<< HEAD
 let tally1 = document.getElementById("duelist1-lp");
 let tally2 = document.getElementById("duelist2-lp");
-=======
-let tally1 = document.getElementById('point-text1');
-let tally2 = document.getElementById('point-text2');
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
 let startYugi = document.getElementById('yugi-btn')
 let startKaiba = document.getElementById('kaiba-btn')
 let startMarik = document.getElementById('marik-btn')
@@ -22,7 +17,6 @@ let duelist2AtkPanel = document.getElementById("duelist2-panel-atk")
 let duelist1Avatar = document.getElementById("duelist1-avatar")
 let duelist2Avatar = document.getElementById("duelist2-avatar")
 
-<<<<<<< HEAD
 let yugiCards = [{
     cardName: "Dark Magician",
     attackPoints: 2500,
@@ -152,98 +146,6 @@ let maiCards = [{
     attackPoints: 1800,
     img: "/images/cardimages/dunames dark witch.png"
 }];
-=======
-let characterCards = {
-    yugiCards: [{
-        cardName: "Dark Magician",
-        attackPoints: 2500
-    },
-    {
-        cardName: "Dark Magician Girl",
-        attackPoints: 2000
-    },
-    {
-        cardName: "Celtic Guardian",
-        attackPoints: 1400
-    },
-    {
-        cardName: "Buster Blader",
-        attackPoints: 2600
-    },
-    {
-        cardName: "Breaker The Magical Warrior",
-        attackPoints: 1600
-    },
-    {
-        cardName: "Dark Magician The Dragon Knight",
-        attackPoints: 3000
-    }],
-    kaibaCards: [{
-        cardName: "Blue Eyes White Dragon",
-        attackPoints: 3000
-    },
-    {
-        cardName: "Battle Ox",
-        attackPoints: 1700
-    },
-    {
-        cardName: "Lord of D",
-        attackPoints: 1200
-    },
-    {
-        cardName: "Kaiser Vorse Raider",
-        attackPoints: 1900
-    },
-    {
-        cardName: "Neo Kaiser Glider",
-        attackPoints: 2400
-    },
-    {
-        cardName: "Dragon Spirit of White",
-        attackPoints: 2500
-    }],
-    joeyCards: [{
-        cardName: "Red Eyes Black Dragon",
-        attackPoints: 2400
-    },
-    {
-        cardName: "Gearfried The Iron Knight",
-        attackPoints: 1800
-    },
-    {
-        cardName: "Gilford The Lightning",
-        attackPoints: 2800
-    },
-    {
-        cardName: "Axe Raider",
-        attackPoints: 1700
-    }],
-    marikCards: [{
-        cardName: "Revival Jam",
-        attackPoints: 1500
-    },
-    {
-        cardName: "Masked Beast Des Gardius",
-        attackPoints: 3300
-    },
-    {
-        cardName: "Helpoemer",
-        attackPoints: 2000
-    }],
-    maiCards: [{
-        cardName: "Cyber Harpie Lady",
-        attackPoints: 1800
-    },
-    {
-        cardName: "Cyber Slash Harpie Lady",
-        attackPoints: 2600
-    },
-    {
-        cardName: "Harpie Queen",
-        attackPoints: 1900
-    }]
-}
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
 
 class Duelist {
     constructor(name, _cards, avatar) {
@@ -254,19 +156,11 @@ class Duelist {
         }   
 }
 
-<<<<<<< HEAD
 const yugi = new Duelist("Yugi", yugiCards, '/images/dm01.png')
 const kaiba = new Duelist("Kaiba", kaibaCards, '/images/dm02.png')
 const marik = new Duelist("Marik", marikCards, '/images/dm10.png')
 const joey = new Duelist("Joey", joeyCards, '/images/dm03.png')
 const mai = new Duelist("Mai", maiCards, '/images/dm04.png')
-=======
-const yugi = new Duelist("Yugi", characterCards.yugiCards)
-const kaiba = new Duelist("Kaiba", characterCards.kaibaCards)
-const marik = new Duelist("Marik", characterCards.marikCards)
-const joey = new Duelist("Joey", characterCards.joeyCards)
-const mai = new Duelist("Mai", characterCards.maiCards)
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
 
 let characterArray = [yugi, kaiba, marik, joey, mai]
 let duelingOpponent = characterArray[Math.floor(Math.random() * 5)]   
@@ -287,29 +181,17 @@ const duel = (duelist1, duelist2) => {
         result1.innerHTML = `${duelist1.name} drew ${duelist1Cards.cardName}`
         result2.innerHTML = `${duelist2.name} drew  ${duelist2Cards.cardName}`
         battleText.innerHTML = ""
-<<<<<<< HEAD
         tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
         tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
         setTimeout(battlePhase, 1500)
-=======
-        tally1.innerHTML = `${duelist1.name}'s LifePoints: ${duelist1.lifePoints}`;
-        tally2.innerHTML = `${duelist2.name}'s LifePoints: ${duelist2.lifePoints}`;
-        setTimeout(battlePhase, 1700)
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
         //----
         function battlePhase() {
             result1.innerHTML = `${duelist1.name} with ${duelist1Cards.cardName}`;
             result2.innerHTML = `${duelist2.name} with  ${duelist2Cards.cardName}`
             battleText.innerHTML = "Declared Battle"
-<<<<<<< HEAD
             tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
             tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
             setTimeout(calculation, 1500)
-=======
-            tally1.innerHTML = `${duelist1.name}'s LifePoints: ${duelist1.lifePoints}`;
-            tally2.innerHTML = `${duelist2.name}'s LifePoints: ${duelist2.lifePoints}`;
-            setTimeout(calculation, 1700)
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
         }
         function calculation() {
             let attackDifference = Math.abs(duelist1Cards.attackPoints - duelist2Cards.attackPoints)
@@ -321,7 +203,6 @@ const duel = (duelist1, duelist2) => {
                 console.log(`${duelist1.name}'s LifePoints: ${duelist1.lifePoints}`);
                 result1.innerHTML = `${duelist1.name} with ${duelist1Cards.cardName}`;
                 battleText.innerHTML = "Battled";
-<<<<<<< HEAD
                 result2.innerHTML = `${duelist2.name}'s ${duelist2Cards.cardName}`;
                 tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
                 tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
@@ -330,13 +211,6 @@ const duel = (duelist1, duelist2) => {
                     result1.innerHTML = `${duelist1.name} is The King of Games`
                     result2.innerHTML = `${duelist2.name} is DEFEATED`
                     battleText.innerHTML = ""
-=======
-                result2.innerHTML = `${duelist2.name}'s ${duelist2Cards.cardName} atk ${duelist2Cards.attackPoints} and lost -${attackDifference}.`;
-                tally1.innerHTML = `${duelist1.name}'s LifePoints: ${duelist1.lifePoints}`;
-                tally2.innerHTML = `${duelist2.name}'s LifePoints: ${duelist2.lifePoints}`;
-                if(duelist2.lifePoints <= 0) {
-                    duelist1Wins()
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
                 }
             } if (duelist2Cards.attackPoints > duelist1Cards.attackPoints) {
                 duelist1.lifePoints = duelist1.lifePoints - attackDifference; 
@@ -348,14 +222,10 @@ const duel = (duelist1, duelist2) => {
                 tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
                 tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
                 if(duelist1.lifePoints <= 0) {
-<<<<<<< HEAD
                     tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
                     result2.innerHTML = `${duelist2.name} is The King of Games`
                     result1.innerHTML = `${duelist1.name} is DEFEATED`
                     battleText.innerHTML = ""
-=======
-                    duelist2Wins()
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
                 }
             } else if (duelist1Cards.attackPoints === duelist2Cards.attackPoints) {
                 result1.innerHTML = "Draw"
@@ -363,7 +233,6 @@ const duel = (duelist1, duelist2) => {
                 
             } else if (duelist1Cards.attackPoints > duelist2Cards.attackPoints && duelist2.lifePoints <= 0) { 
                 duelist2.lifePoints = duelist2.lifePoints - attackDifference; 
-<<<<<<< HEAD
                 tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
                 result1.innerHTML = `${duelist1.name} is The King of Games`
                 result2.innerHTML = `${duelist2.name} is DEFEATED`
@@ -372,12 +241,6 @@ const duel = (duelist1, duelist2) => {
                 tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`
                 result1.innerHTML = `${duelist2.name} is DEFEATED`
                 result2.innerHTML = `${duelist2.name} is The King of Games`
-=======
-                duelist1Wins()
-           } else if (duelist2Cards.attackPoints > duelist1Cards.attackPoints && duelist1.lifePoints <= 0) {
-               duelist1.lifePoints = duelist1.lifePoints - attackDifference; 
-                duelist2Wins()
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
           } if (duelist1.lifePoints > 0 && duelist2.lifePoints > 0) {
             setTimeout(duelStart, 1700)
                   
@@ -400,16 +263,12 @@ const duel = (duelist1, duelist2) => {
     }
 }
 
-<<<<<<< HEAD
 
-startYugi.addEventListener('click', () => {
+/* startYugi.addEventListener('click', () => {
     duel(yugi, characterArray[Math.floor(Math.random() * 5)])
-=======
-function displayPoints() {
->>>>>>> ae5afc6d10f7ba3f9948c3d7b0a4e917886ba23c
     buttonContainer.classList.add('hide')
     pointsContainer.style.display ="flex"
-}
+}) */
 
 const startBtns = [startYugi, startKaiba, startMarik, startJoey, startMai]
 

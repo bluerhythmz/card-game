e5334d0ca19456cc5541c0a281a402c9fbcce46a
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-07-23 14:41:31 -0700

    refactored to clean up variables

--- cards.js
+++ cards.js
@@ -1,129 +1,129 @@
 export let yugiCards = [{
     cardName: "Dark Magician",
     attackPoints: 2500,
-    img: "/images/cardimages/dark magician.jpg"
+    img: "./images/cardimages/dark magician.jpg"
 },
 {
     cardName: "Dark Magician Girl",
     attackPoints: 2000,
-    img: "/images/cardimages/dark magician girl.jpg"
+    img: "./images/cardimages/dark magician girl.jpg"
 },
 {
     cardName: "Celtic Guardian",
     attackPoints: 1400,
-    img: "/images/cardimages/celtic guardian.jpg"
+    img: "./images/cardimages/celtic guardian.jpg"
 },
 {
     cardName: "Buster Blader",
     attackPoints: 2600,
-    img: "/images/cardimages/buster blader.jpg"
+    img: "./images/cardimages/buster blader.jpg"
 },
 {
     cardName: "Dark Magician The Dragon Knight",
     attackPoints: 3000,
-    img: "/images/cardimages/dark magician the dragon knight.jpg"
+    img: "./images/cardimages/dark magician the dragon knight.jpg"
 }]
 
 export let kaibaCards = [{
     cardName: "Blue Eyes White Dragon",
     attackPoints: 3000,
-    img: "/images/cardimages/blue eyes white dragon.jpg"
+    img: "./images/cardimages/blue eyes white dragon.jpg"
 },
 {
     cardName: "Battle Ox",
     attackPoints: 1700,
-    img: "/images/cardimages/battle ox.jpg"
+    img: "./images/cardimages/battle ox.jpg"
 },
 {
     cardName: "Lord of D",
     attackPoints: 1200,
-    img: "/images/cardimages/lord of d.jpg"
+    img: "./images/cardimages/lord of d.jpg"
 },
 {
     cardName: "Kaiser Vorse Raider",
     attackPoints: 1900,
-    img: "/images/cardimages/kaiser vorse raider.jpg"
+    img: "./images/cardimages/kaiser vorse raider.jpg"
 },
 {
     cardName: "Dragon Spirit of White",
     attackPoints: 2500,
-    img: "/images/cardimages/dragon spirit of white.jpg"
+    img: "./images/cardimages/dragon spirit of white.jpg"
 }];
 
 export let joeyCards = [{
     cardName: "Red Eyes Black Dragon",
     attackPoints: 2400,
-    img: "/images/cardimages/red eyes black dragon.jpg"
+    img: "./images/cardimages/red eyes black dragon.jpg"
 },
 {
     cardName: "Gearfried The Iron Knight",
     attackPoints: 1800,
-    img: "/images/cardimages/gearfried.jpg"
+    img: "./images/cardimages/gearfried.jpg"
 },
 {
     cardName: "Gilford The Lightning",
     attackPoints: 2800,
-    img: "/images/cardimages/gilford the lightning.jpg"
+    img: "./images/cardimages/gilford the lightning.jpg"
 },
 {
     cardName: "Axe Raider",
     attackPoints: 1700,
-    img: "/images/cardimages/axe raider.jpg"
+    img: "./images/cardimages/axe raider.jpg"
 },
 {
     cardName: "Black Skull Dragon",
     attackPoints: 3200,
-    img: "/images/cardimages/black skull dragon.jpg"
+    img: "./images/cardimages/black skull dragon.jpg"
 }];
 
 export let marikCards = [{
     cardName: "Revival Jam",
     attackPoints: 1500,
-    img: "/images/cardimages/revival jam.jpg"
+    img: "./images/cardimages/revival jam.jpg"
 },
 {
     cardName: "Masked Beast Des Gardius",
     attackPoints: 3300,
-    img: "/images/cardimages/masked beast.jpg"
+    img: "./images/cardimages/masked beast.jpg"
 },
 {
     cardName: "Helpoemer",
     attackPoints: 2000,
-    img: "/images/cardimages/helpoemer.jpg"
+    img: "./images/cardimages/helpoemer.jpg"
 },
 {
     cardName: "Lava Golem",
     attackPoints: 3000,
-    img: "/images/cardimages/lava golem.jpg"
+    img: "./images/cardimages/lava golem.jpg"
 },
 {
     cardName: "Legendary Fiend",
     attackPoints: 1500,
-    img: "/images/cardimages/legendary fiend.jpg"
+    img: "./images/cardimages/legendary fiend.jpg"
 }];
 
 export let maiCards = [{
     cardName: "Cyber Harpie Lady",
     attackPoints: 1800,
-    img: "/images/cardimages/cyber harpie.jpg"
+    img: "./images/cardimages/cyber harpie.jpg"
 },
 {
     cardName: "Cyber Slash Harpie Lady",
     attackPoints: 2600,
-    img: "/images/cardimages/cyber slash harpie.jpg"
+    img: "./images/cardimages/cyber slash harpie.jpg"
 },
 {
     cardName: "Harpie Queen",
     attackPoints: 1900,
-    img: "/images/cardimages/harpie queen.jpg"
+    img: "./images/cardimages/harpie queen.jpg"
 },
 {
     cardName: "Harpie's Pet Dragon",
     attackPoints: 2000,
-    img: "/images/cardimages/harpies pet dragon.jpg"
+    img: "./images/cardimages/harpies pet dragon.jpg"
 },
 {
     cardName: "Dunames Dark Witch",
     attackPoints: 1800,
-    img: "/images/cardimages/dunames dark witch.jpg"
+    img: "./images/cardimages/dunames dark witch.jpg"
 }];
--- main.js
+++ main.js
@@ -1,34 +1,59 @@
-import { yugiCards, kaibaCards, joeyCards, marikCards, maiCards } from './cards.js'
+import {
+  yugiCards,
+  kaibaCards,
+  joeyCards,
+  marikCards,
+  maiCards,
+} from "./cards.js";
 
-const startBtns = Array.from(document.querySelectorAll('[data-button]'))
-const buttonContainer = document.querySelector('.button-container')
-let d1Panel = document.getElementById('d1Panel')
-let d2Panel = document.getElementById('d2Panel')
-let logo = document.getElementById('logo')
-let d1Cover = document.querySelector('#d1-cover')
-let d2Cover = document.querySelector('#d2-cover')
-let duelMoves = document.querySelector('.duel-moves')
+const startBtns = Array.from(document.querySelectorAll("[data-button]"));
+const buttonContainer = document.querySelector(".button-container");
+let d1Panel = document.getElementById("d1Panel");
+let d2Panel = document.getElementById("d2Panel");
+let logo = document.getElementById("logo");
+let d1Cover = document.querySelector("#d1-cover");
+let d2Cover = document.querySelector("#d2-cover");
+let duelMoves = document.querySelector(".duel-moves");
 
 class Duelist {
-    constructor(name, _cards, avatar) {
-        this.name = name;
-        this.cards = _cards;
-        this.avatar = avatar
-        this.lifePoints = 8000;
-        }   
+  constructor(name, _cards, avatar) {
+    this.name = name;
+    this.cards = _cards;
+    this.avatar = avatar;
+    this.lifePoints = 8000;
+  }
 }
 
-const yugi = new Duelist("Yugi", yugiCards, '/images/dm01.png')
-const kaiba = new Duelist("Kaiba", kaibaCards, '/images/dm02.png')
-const marik = new Duelist("Marik", marikCards, '/images/dm10.png')
-const joey = new Duelist("Joey", joeyCards, '/images/dm03.png')
-const mai = new Duelist("Mai", maiCards, '/images/dm04.png')
+const field = {
+  playerOneField: {
+    card: document.getElementById("duelist1-monster"),
+    cardImage: document.getElementById("duelist1-card"),
+    attackPoints: document.getElementById("duelist1-panel-atk"),
+    avatarElement: document.getElementById("duelist1-avatar"),
+    battleResultElement: document.getElementById("result1"),
+    lifePointsElement: document.getElementById("duelist1-lp"),
+  },
+  playerTwoField: {
+    card: document.getElementById("duelist2-monster"),
+    cardImage: document.getElementById("duelist2-card"),
+    attackPoints: document.getElementById("duelist2-panel-atk"),
+    avatarElement: document.getElementById("duelist2-avatar"),
+    battleResultElement: document.getElementById("result2"),
+    lifePointsElement: document.getElementById("duelist2-lp"),
+  },
+};
 
-let duelistArray = [yugi, kaiba, marik, joey, mai]
-let duelingOpponent = duelistArray[Math.floor(Math.random() * 5)]
+const yugi = new Duelist("Yugi", yugiCards, "./images/dm01.png");
+const kaiba = new Duelist("Kaiba", kaibaCards, "./images/dm02.png");
+const marik = new Duelist("Marik", marikCards, "./images/dm10.png");
+const joey = new Duelist("Joey", joeyCards, "./images/dm03.png");
+const mai = new Duelist("Mai", maiCards, "./images/dm04.png");
+
+let duelistArray = [yugi, kaiba, marik, joey, mai];
+let duelingOpponent = duelistArray[Math.floor(Math.random() * 5)];
 
 const duel = (duelist1, duelist2) => {
-    let d1FieldCardElement = document.getElementById('duelist1-monster')
+  /* let d1FieldCardElement = document.getElementById('duelist1-monster')
     let d2FieldCardElement = document.getElementById('duelist2-monster')
     let duelist1AtkPanel = document.getElementById("duelist1-panel-atk")
     let duelist2AtkPanel = document.getElementById("duelist2-panel-atk")
@@ -38,60 +63,62 @@ const duel = (duelist1, duelist2) => {
     let duelist2CardImage = document.getElementById('duelist2-card')
     let result1 = document.getElementById('result1')
     let result2 = document.getElementById('result2')
-    let battleText = document.getElementById('battleText')
     let tally1 = document.getElementById("duelist1-lp")
-    let tally2 = document.getElementById("duelist2-lp")
+    let tally2 = document.getElementById("duelist2-lp") */
+  let battleText = document.getElementById("battleText");
 
-    duelStart()
-    function duelStart() {
-        
-        let duelist1Cards = duelist1.cards[Math.floor(Math.random() * 5)] 
-        let duelist2Cards = duelist2.cards[Math.floor(Math.random() * 5)]
-        duelMoves.style.display = "flex"
-        duelist1CardImage.src = duelist1Cards.img
-        duelist2CardImage.src = duelist2Cards.img
-        duelist1Avatar.src = duelist1.avatar
-        duelist2Avatar.src = duelist2.avatar
-        duelist1AtkPanel.innerHTML = `ATK: ${duelist1Cards.attackPoints}`
-        duelist2AtkPanel.innerHTML = `ATK: ${duelist2Cards.attackPoints}`
-        result1.innerHTML = ""
-        d1FieldCardElement.src = duelist1Cards.img
-        d2FieldCardElement.src = duelist2Cards.img
-        battleText.innerHTML = "VS"
-        result2.innerHTML = ""
-        tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`
-        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-        setTimeout(calculation, 1800)
-        
-        function calculation() {
-            let attackDifference = Math.abs(duelist1Cards.attackPoints - duelist2Cards.attackPoints)
-            if (duelist1Cards.attackPoints > duelist2Cards.attackPoints) {
-               
-                let finalVal = duelist2.lifePoints - attackDifference;
-                let decrement = 25
-                result2.innerHTML = `${duelist2.name} loses -${attackDifference} Life Points!`;
-                d2FieldCardElement.src = ""
-                battleText.innerHTML = ""
-                result1.innerHTML = ""
-                console.log(duelist2.lifePoints)
-                console.log(finalVal)
-                const counterDecrementer = () => {
-                    if (duelist2.lifePoints > finalVal) {
-                        tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`
-                        tally2.innerHTML = `${duelist2.name}: ${Math.ceil(duelist2.lifePoints -= decrement)}`
-                        setTimeout(counterDecrementer, 10)
-                    } else {
-                        duelist2.lifePoints = finalVal
-                        tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`
-                        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-                    }
-                }
-                counterDecrementer()
-                
-                setTimeout(checkForWin, 1000)
-                
-            } if (duelist2Cards.attackPoints > duelist1Cards.attackPoints) {
-                /* duelist1.lifePoints = duelist1.lifePoints - attackDifference; 
+  duelStart();
+  function duelStart() {
+    let duelist1Cards = duelist1.cards[Math.floor(Math.random() * 5)];
+    let duelist2Cards = duelist2.cards[Math.floor(Math.random() * 5)];
+    duelMoves.style.display = "flex";
+    field.playerOneField.cardImage.src = duelist1Cards.img;
+    field.playerTwoField.cardImage.src = duelist2Cards.img;
+    field.playerOneField.avatarElement.src = duelist1.avatar;
+    field.playerTwoField.avatarElement.src = duelist2.avatar;
+    field.playerOneField.attackPoints.innerHTML = `ATK: ${duelist1Cards.attackPoints}`;
+    field.playerTwoField.attackPoints.innerHTML = `ATK: ${duelist2Cards.attackPoints}`;
+    field.playerOneField.battleResultElement.innerHTML = "";
+    field.playerOneField.card.src = duelist1Cards.img;
+    field.playerTwoField.card.src = duelist2Cards.img;
+    battleText.innerHTML = "VS";
+    field.playerTwoField.battleResultElement.innerHTML = "";
+    field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+    field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+    setTimeout(calculation, 1800);
+
+    function calculation() {
+      let attackDifference = Math.abs(
+        duelist1Cards.attackPoints - duelist2Cards.attackPoints
+      );
+      if (duelist1Cards.attackPoints > duelist2Cards.attackPoints) {
+        let finalVal = duelist2.lifePoints - attackDifference;
+        let decrement = 25;
+        field.playerTwoField.battleResultElement.innerHTML = `${duelist2.name} loses -${attackDifference} Life Points!`;
+        field.playerTwoField.card.src = "";
+        battleText.innerHTML = "";
+        field.playerOneField.battleResultElement.innerHTML = "";
+        console.log(duelist2.lifePoints);
+        console.log(finalVal);
+        const counterDecrementer = () => {
+          if (duelist2.lifePoints > finalVal) {
+            field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+            field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${Math.ceil(
+              (duelist2.lifePoints -= decrement)
+            )}`;
+            setTimeout(counterDecrementer, 10);
+          } else {
+            duelist2.lifePoints = finalVal;
+            field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+            field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+          }
+        };
+        counterDecrementer();
+
+        setTimeout(checkForWin, 1000);
+      }
+      if (duelist2Cards.attackPoints > duelist1Cards.attackPoints) {
+        /* duelist1.lifePoints = duelist1.lifePoints - attackDifference; 
                 result1.innerHTML = `${duelist1.name} loses -${attackDifference} Life Points!`
                 result2.innerHTML = ""
                 battleText.innerHTML = ""
@@ -99,140 +126,143 @@ const duel = (duelist1, duelist2) => {
                 tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
                 tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
                 setTimeout(checkForWin, 1000) */
-                let finalVal2 = duelist1.lifePoints - attackDifference;
-                let decrement2 = 25
-                result1.innerHTML = `${duelist1.name} loses -${attackDifference} Life Points!`;
-                d1FieldCardElement.src = ""
-                battleText.innerHTML = ""
-                result2.innerHTML = ""
-               
-                const counterDecrementer2 = () => {
-                    if (duelist1.lifePoints > finalVal2) {
-                        tally1.innerHTML = `${duelist1.name}: ${Math.ceil(duelist1.lifePoints -= decrement2)}`
-                        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-                        setTimeout(counterDecrementer2, 1)
-                    } else {
-                        duelist1.lifePoints = finalVal2
-                        tally1.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`
-                        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-                    }
-                }
-                counterDecrementer2()
-                
-                setTimeout(checkForWin, 1000)
-            } else if (duelist1Cards.attackPoints === duelist2Cards.attackPoints) {
-                result1.innerHTML = ""
-                result2.innerHTML = ""
-                battleText.innerHTML = "Draw!"
-                setTimeout(checkForWin, 1000)   
-            }
-        } 
-    }
+        let finalVal2 = duelist1.lifePoints - attackDifference;
+        let decrement2 = 25;
+        field.playerOneField.battleResultElement.innerHTML = `${duelist1.name} loses -${attackDifference} Life Points!`;
+        field.playerOneField.card.src = "";
+        battleText.innerHTML = "";
+        field.playerTwoField.battleResultElement.innerHTML = "";
 
-    const checkForWin = () => {
-        if(duelist2.lifePoints <= 0) {
-            duelist1Wins()
-        } else if (duelist1.lifePoints <= 0) {
-            duelist2Wins()
-        } else {
-            setTimeout(duelStart, 1800)
-        }
-    }
+        const counterDecrementer2 = () => {
+          if (duelist1.lifePoints > finalVal2) {
+            field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${Math.ceil(
+              (duelist1.lifePoints -= decrement2)
+            )}`;
+            field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+            setTimeout(counterDecrementer2, 1);
+          } else {
+            duelist1.lifePoints = finalVal2;
+            field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+            field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+          }
+        };
+        counterDecrementer2();
 
-    const duelist1Wins = () => {
-        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-        result1.innerHTML = `${duelist1.name} is The King of Games`
-        result2.innerHTML = ""
-        battleText.innerHTML = ""
-        d2FieldCardElement.src = ""
-        d1FieldCardElement.src = duelist1.avatar
-        d2Cover.style.display = "block"
-        d2Cover.style.background = "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))"
-        reset()
+        setTimeout(checkForWin, 1000);
+      } else if (duelist1Cards.attackPoints === duelist2Cards.attackPoints) {
+        field.playerOneField.battleResultElement.innerHTML = "";
+        field.playerTwoField.battleResultElement.innerHTML = "";
+        battleText.innerHTML = "Draw!";
+        setTimeout(checkForWin, 1000);
+      }
     }
-    
-    const duelist2Wins = () => {
-        tally2.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`
-        result2.innerHTML = `${duelist2.name} is The King of Games`
-        result1.innerHTML = ""
-        battleText.innerHTML = ""
-        d1FieldCardElement.src = ""
-        d2FieldCardElement.src = duelist2.avatar
-        d1Cover.style.display = "block"
-        d1Cover.style.background = "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))"
-        reset()
-        
-    }
+  }
 
-    function reset() {
-        const resetButton = document.createElement('button')
-        resetButton.classList.add('reset-btn')
-        resetButton.innerHTML = "Reset"
-        battleText.appendChild(resetButton)
-        
-        resetButton.addEventListener('click', () => {
-            buttonContainer.classList.remove('hide')
-            d1Panel.style.display ="none"
-            d2Panel.style.display ="none"
-            d1Cover.style.display = "none"
-            d2Cover.style.display = "none"
-            result1.innerHTML = ""
-            result2.innerHTML = ""
-            battleText.removeChild(resetButton)
-            logo.style.display = "block"
-            d1FieldCardElement.src = ""
-            d2FieldCardElement.src = ""
-            startGame()
-        })
+  const checkForWin = () => {
+    if (duelist2.lifePoints <= 0) {
+      duelist1Wins();
+    } else if (duelist1.lifePoints <= 0) {
+      duelist2Wins();
+    } else {
+      setTimeout(duelStart, 1800);
     }
+  };
 
-    function startGame() {
-        buttonContainer.classList.remove('hide')
-        d1Panel.style.display ="none"
-        d2Panel.style.display ="none"
-        duelist1.lifePoints = 8000
-        duelist2.lifePoints = 8000
-        d1FieldCardElement.src = ""
-        d2FieldCardElement.src = ""
-        duelMoves.style.display = "none"
-    }
-}
+  const duelist1Wins = () => {
+    field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+    field.playerOneField.battleResultElement.innerHTML = `${duelist1.name} is The King of Games`;
+    field.playerTwoField.battleResultElement.innerHTML = "";
+    battleText.innerHTML = "";
+    field.playerTwoField.card.src = "";
+    field.playerOneField.card.src = duelist1.avatar;
+    d2Cover.style.display = "block";
+    d2Cover.style.background =
+      "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))";
+    reset();
+  };
+
+  const duelist2Wins = () => {
+    field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+    field.playerTwoField.battleResultElement.innerHTML = `${duelist2.name} is The King of Games`;
+    field.playerOneField.battleResultElement.innerHTML = "";
+    battleText.innerHTML = "";
+    field.playerOneField.card.src = "";
+    field.playerTwoField.card.src = duelist2.avatar;
+    d1Cover.style.display = "block";
+    d1Cover.style.background =
+      "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))";
+    reset();
+  };
+
+  function reset() {
+    const resetButton = document.createElement("button");
+    resetButton.classList.add("reset-btn");
+    resetButton.innerHTML = "Reset";
+    battleText.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      buttonContainer.classList.remove("hide");
+      d1Panel.style.display = "none";
+      d2Panel.style.display = "none";
+      d1Cover.style.display = "none";
+      d2Cover.style.display = "none";
+      field.playerOneField.battleResultElement.innerHTML = "";
+      field.playerTwoField.battleResultElement.innerHTML = "";
+      battleText.removeChild(resetButton);
+      logo.style.display = "block";
+      field.playerOneField.card.src = "";
+      field.playerTwoField.card.src = "";
+      startGame();
+    });
+  }
+
+  function startGame() {
+    buttonContainer.classList.remove("hide");
+    d1Panel.style.display = "none";
+    d2Panel.style.display = "none";
+    duelist1.lifePoints = 8000;
+    duelist2.lifePoints = 8000;
+    field.playerOneField.card.src = "";
+    field.playerTwoField.card.src = "";
+    duelMoves.style.display = "none";
+  }
+};
 
 function displayPoints() {
-    buttonContainer.classList.add('hide')
-    d1Panel.style.display ="flex"
-    d2Panel.style.display ="flex"
+  buttonContainer.classList.add("hide");
+  d1Panel.style.display = "flex";
+  d2Panel.style.display = "flex";
 }
 
 startBtns.forEach((btn, i) => {
-    function playerSelect(player, opponent) {    
-        btn.addEventListener('click', () => {
-            while (player === opponent) {
-                duelistArray.pop(opponent)
-                opponent = duelistArray[Math.floor(Math.random() * 4)]
-            }
-            logo.style.display = "none"
-            duel(player, opponent)
-            displayPoints()
-        })
-    }
-    switch (i) {
-        case 0:
-            playerSelect(yugi, duelingOpponent)
-            break;
-        case 1:
-            playerSelect(kaiba, duelingOpponent)
-            break;
-        case 2:
-            playerSelect(marik, duelingOpponent)
-            break;
-        case 3:
-            playerSelect(joey, duelingOpponent)
-            break;
-        case 4:
-            playerSelect(mai, duelingOpponent)
-            break;
-        default:
-            alert("error")
-    } 
-})
+  function playerSelect(player, opponent) {
+    btn.addEventListener("click", () => {
+      while (player === opponent) {
+        duelistArray.pop(opponent);
+        opponent = duelistArray[Math.floor(Math.random() * 4)];
+      }
+      logo.style.display = "none";
+      duel(player, opponent);
+      displayPoints();
+    });
+  }
+  switch (i) {
+    case 0:
+      playerSelect(yugi, duelingOpponent);
+      break;
+    case 1:
+      playerSelect(kaiba, duelingOpponent);
+      break;
+    case 2:
+      playerSelect(marik, duelingOpponent);
+      break;
+    case 3:
+      playerSelect(joey, duelingOpponent);
+      break;
+    case 4:
+      playerSelect(mai, duelingOpponent);
+      break;
+    default:
+      alert("error");
+  }
+});

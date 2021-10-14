5fe05f0452cf6b363b8e4bbb5d4ea818d13e5046
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-10-13 17:30:34 -0700

    cleaning functions

--- main.js
+++ main.js
@@ -12,16 +12,18 @@ let introScreen = document.querySelector(".intro-screen");
 let gameScreen = document.querySelector(".game-screen");
 
 class Duelist {
-  constructor(name, _cards, avatar) {
+  constructor(name, _cards, avatar, chosenCard) {
     this.name = name;
     this.cards = _cards;
     this.avatar = avatar;
     this.lifePoints = 8000;
+    this.chosenCard = chosenCard;
   }
 }
 
 const field = {
   playerOneField: {
+    lifePoints: {},
     card: document.getElementById("duelist1-monster"),
     cardImage: document.getElementById("duelist1-card"),
     attackPoints: document.getElementById("duelist1-panel-atk"),
@@ -48,7 +50,136 @@ const mai = new Duelist("Mai", decks.maiCards, "./images/dm04.png");
 let duelistArray = [yugi, kaiba, marik, joey, mai];
 let duelingOpponent = duelistArray[Math.floor(Math.random() * 5)];
 
-const duel = (duelist1, duelist2) => {
+function duelStart(duelist1, duelist2) {
+  let battleText = document.getElementById("battleText");
+  duelist1.chosenCard = duelist1.cards[Math.floor(Math.random() * 5)];
+  duelist2.chosenCard = duelist2.cards[Math.floor(Math.random() * 5)];
+  duelMoves.style.display = "flex";
+  field.playerOneField.cardImage.src = duelist1.chosenCard.img;
+  field.playerTwoField.cardImage.src = duelist2.chosenCard.img;
+  field.playerOneField.avatarElement.src = duelist1.avatar;
+  field.playerTwoField.avatarElement.src = duelist2.avatar;
+  field.playerOneField.attackPoints.innerHTML = `ATK: ${duelist1.chosenCard.attackPoints}`;
+  field.playerTwoField.attackPoints.innerHTML = `ATK: ${duelist2.chosenCard.attackPoints}`;
+  field.playerOneField.battleResultElement.innerHTML = "";
+  field.playerOneField.card.src = duelist1.chosenCard.img;
+  field.playerTwoField.card.src = duelist2.chosenCard.img;
+  battleText.innerHTML = "VS";
+  field.playerTwoField.battleResultElement.innerHTML = "";
+  field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+  field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+  setTimeout(() => {
+    calculation(duelist1, duelist2);
+  }, 1800);
+}
+
+function calculation(duelist1, duelist2) {
+  let attackDifference = Math.abs(
+    duelist1.chosenCard.attackPoints - duelist2.chosenCard.attackPoints
+  );
+  if (duelist1.chosenCard.attackPoints > duelist2.chosenCard.attackPoints) {
+    let finalVal = duelist2.lifePoints - attackDifference;
+    let decrement = 25;
+    field.playerTwoField.battleResultElement.innerHTML = `${duelist2.name} loses -${attackDifference} Life Points!`;
+    field.playerTwoField.card.src = "";
+    battleText.innerHTML = "";
+    field.playerOneField.battleResultElement.innerHTML = "";
+    const counterDecrementer = (duelist1, duelist2) => {
+      if (duelist2.lifePoints > finalVal) {
+        field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+        field.playerTwoField.lifePointsElement.innerHTML = `${
+          duelist2.name
+        }: ${Math.ceil((duelist2.lifePoints -= decrement))}`;
+        setTimeout(counterDecrementer(duelist1, duelist2), 10);
+      } else {
+        duelist2.lifePoints = finalVal;
+        field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+        field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+      }
+    };
+    counterDecrementer(duelist1, duelist2);
+
+    setTimeout(() => {
+      checkForWin(duelist1, duelist2);
+    }, 1000);
+  }
+  if (duelist2.chosenCard.attackPoints > duelist1.chosenCard.attackPoints) {
+    let finalVal2 = duelist1.lifePoints - attackDifference;
+    let decrement2 = 25;
+    field.playerOneField.battleResultElement.innerHTML = `${duelist1.name} loses -${attackDifference} Life Points!`;
+    field.playerOneField.card.src = "";
+    battleText.innerHTML = "";
+    field.playerTwoField.battleResultElement.innerHTML = "";
+
+    const counterDecrementer2 = (duelist1, duelist2) => {
+      if (duelist1.lifePoints > finalVal2) {
+        field.playerOneField.lifePointsElement.innerHTML = `${
+          duelist1.name
+        }: ${Math.ceil((duelist1.lifePoints -= decrement2))}`;
+        field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+        setTimeout(counterDecrementer2(duelist1, duelist2), 10);
+      } else {
+        duelist1.lifePoints = finalVal2;
+        field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+        field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+      }
+    };
+    counterDecrementer2(duelist1, duelist2);
+
+    setTimeout(() => {
+      checkForWin(duelist1, duelist2);
+    }, 1000);
+  } else if (
+    duelist1.chosenCard.attackPoints === duelist2.chosenCard.attackPoints
+  ) {
+    field.playerOneField.battleResultElement.innerHTML = "";
+    field.playerTwoField.battleResultElement.innerHTML = "";
+    battleText.innerHTML = "Draw!";
+    setTimeout(() => {
+      checkForWin(duelist1, duelist2);
+    }, 1000);
+  }
+}
+
+const checkForWin = (duelist1, duelist2) => {
+  if (duelist2.lifePoints <= 0) {
+    duelist1Wins(duelist1, duelist2);
+  } else if (duelist1.lifePoints <= 0) {
+    duelist2Wins(duelist1, duelist2);
+  } else {
+    setTimeout(() => {
+      duelStart(duelist1, duelist2);
+    }, 1800);
+  }
+};
+
+const duelist1Wins = (duelist1, duelist2) => {
+  field.playerTwoField.lifePointsElement.innerHTML = `${duelist2.name}: ${duelist2.lifePoints}`;
+  field.playerOneField.battleResultElement.innerHTML = `${duelist1.name} is The King of Games`;
+  field.playerTwoField.battleResultElement.innerHTML = "";
+  battleText.innerHTML = "";
+  field.playerTwoField.card.src = "";
+  field.playerOneField.card.src = duelist1.avatar;
+  d2Cover.style.display = "block";
+  d2Cover.style.background =
+    "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))";
+  reset();
+};
+
+const duelist2Wins = (duelist1, duelist2) => {
+  field.playerOneField.lifePointsElement.innerHTML = `${duelist1.name}: ${duelist1.lifePoints}`;
+  field.playerTwoField.battleResultElement.innerHTML = `${duelist2.name} is The King of Games`;
+  field.playerOneField.battleResultElement.innerHTML = "";
+  battleText.innerHTML = "";
+  field.playerOneField.card.src = "";
+  field.playerTwoField.card.src = duelist2.avatar;
+  d1Cover.style.display = "block";
+  d1Cover.style.background =
+    "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8))";
+  reset();
+};
+
+/* const duel = (duelist1, duelist2) => {
   let battleText = document.getElementById("battleText");
 
   duelStart();
@@ -202,7 +333,7 @@ const duel = (duelist1, duelist2) => {
     field.playerTwoField.card.src = "";
     duelMoves.style.display = "none";
   }
-};
+}; */
 
 function displayPoints() {
   buttonContainer.classList.add("hide");
@@ -218,9 +349,9 @@ startBtns.forEach((btn, i) => {
         opponent = duelistArray[Math.floor(Math.random() * 4)];
       }
       logo.style.display = "none";
-      introScreen.style.display = "none"
-      gameScreen.style.display = "flex"
-      duel(player, opponent);
+      introScreen.style.display = "none";
+      gameScreen.style.display = "flex";
+      duelStart(player, opponent);
       displayPoints();
     });
   }
